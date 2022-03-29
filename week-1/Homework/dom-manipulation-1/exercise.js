25c82e7fc7f11cbf8216c8b887f451b898336c06
diegok23 <dkogut@gmail.com>
2022-03-29 20:22:26 +0200

    Ejercicios completados

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -15,8 +15,17 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+const pSelector = document.querySelectorAll("p");
+console.log(pSelector);
 
+const firstDiv = document.querySelector("div");
+console.log(firstDiv);
 
+const firstId = document.querySelector("#jumbotron-text");
+console.log(firstId);
+
+const pSelectorPrimary = document.querySelectorAll(".primary-content p");
+console.log(pSelectorPrimary);
 /*
 Task 2
 ======
@@ -24,6 +33,10 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+const alertButton = document.querySelector("#alertBtn");
+alertButton.addEventListener("click", () => {
+  alert("Thanks for visiting Bikes for Refugees!");
+});
 
 /*
 Task 3
@@ -32,15 +45,25 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
-
+const changeBackground = document.querySelector("#bgrChangeBtn");
+changeBackground.addEventListener("click", () => {
+  const htmlElement = document.querySelector("body");
+  htmlElement.style.backgroundColor = "#0aaaaa";
+});
 /*
+
 Task 4
 ======
 
 When a user clicks the ‘Add some text’ button, a new paragraph should be added inside the section that says “LEARN MORE”
 */
 
-
+const addParagraphButton = document.querySelector("#addTextBtn").addEventListener("click", () => {
+  const sectionToAddParagraph = document.querySelector("#mainArticles");
+  const addParagraph = document.createElement("p");
+  addParagraph.innerText ='Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+  sectionToAddParagraph.appendChild(addParagraph);
+});
 
 /*
 Task 5
@@ -49,6 +72,14 @@ Task 5
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
 
+const lagerTextButton = document
+  .querySelector("#largerLinksBtn")
+  .addEventListener("click", () => {
+    const largerText = Array.from(document.querySelectorAll("a"));
+    largerText.forEach(function (item) {
+      item.style.fontSize = "larger";
+    });
+  });
 
 /*
 Task 6
@@ -59,6 +90,16 @@ When the 'Add' button is clicked, get the text inside the input field and create
 Also clear the text inside the input field
 */
 
+const addArticleButton = document.querySelector('#addArticleBtn');
+addArticleButton.addEventListener('click', () => {
+  const sectionToAddParagraph = document.querySelector('#mainArticles');
+  const addParagraph = document.createElement('p');
+  const addArticle = document.querySelector('.addArticle');
+  addParagraph.innerText = addArticle.value;
+  sectionToAddParagraph.appendChild(addParagraph);
+  addArticle.value = '';
+});
+
 /*
 Task 7
 ======
@@ -68,3 +109,15 @@ Using the same function in Task 3, every time the 'Change colour' button is clic
 The next color when you are in the last color of the array will be the first color again.
 */
 
+const colors = ['#11aaaa', '#22bbbb', '#33cccc', '#44dddd', '#55eeee'];
+const changeArrayBackground = document.querySelector('body');
+let clickIndex = 0;
+
+changeBackground.addEventListener('click', () => {
+  if (clickIndex < colors.length) {
+    clickIndex++;
+  } else {
+    clickIndex = 0;
+  }
+  changeArrayBackground.style.backgroundColor = colors[clickIndex];
+});
---
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -0,0 +1,64 @@
+//## Part 1
+
+const changeColorBlue = document.querySelector('#blueBtn');
+changeColorBlue.addEventListener('click', () => {
+  document.querySelector('.jumbotron').style.backgroundColor = '#588fbd';
+  document.querySelector('.buttons .btn-primary').style.backgroundColor = '#ffa500';
+  document.querySelector('.buttons .btn-secondary').style.backgroundColor = 'black';
+  document.querySelector('.buttons .btn-secondary').style.color = 'white';
+});
+
+const changeColorOrange = document.querySelector('#orangeBtn');
+changeColorOrange.addEventListener('click', () => {
+  document.querySelector('.jumbotron').style.backgroundColor = '#f0ad4e';
+  document.querySelector('.buttons .btn-primary').style.backgroundColor = '#5751fd';
+  document.querySelector('.buttons .btn-secondary').style.backgroundColor = '31b0d5';
+  document.querySelector('.buttons .btn-secondary').style.color = 'white';
+});
+
+const changeColorGreen = document.querySelector('#greenBtn');
+changeColorGreen.addEventListener('click', () => {
+  document.querySelector('.jumbotron').style.backgroundColor = '#87ca8a';
+  document.querySelector('.buttons .btn-primary').style.backgroundColor = 'black';
+  document.querySelector('.buttons .btn-secondary').style.backgroundColor = '8c9c08';
+});
+
+//## Part 2
+
+const email = document.querySelector('exampleInputEmail1');
+const userName = document.querySelector('example-text-input');
+const description = document.querySelector('exampleTextarea');
+const checkForm = document.querySelector('form');
+console.log(exampleInputEmail1);
+
+checkForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = document.querySelector('#exampleInputEmail1');
+  const userName = document.querySelector('#example-text-input');
+  const description = document.querySelector('#exampleTextarea');
+
+  const isValidEmail = email.value.length > 0 && email.value.includes('@');
+  const isValidUserName = userName.value.length > 0;
+  const isValidDescription = description.value.length > 0;
+
+  if (isValidEmail && isValidUserName && isValidDescription) {
+    email.value = '';
+    userName.value = '';
+    description.value = '';
+    email.style.backgroundColor = '#ffffff';
+    userName.style.backgroundColor = '#ffffff';
+    description.style.backgroundColor = '#ffffff';
+    alert('Thanks for filling out the form');
+  } else {
+    if (!isValidEmail) {
+      email.style.backgroundColor = 'red';
+    }
+    if (!isValidUserName) {
+      userName.style.backgroundColor = 'red';
+    }
+    if (!isValidDescription) {
+      description.style.backgroundColor = 'red';
+    }
+  }
+});
