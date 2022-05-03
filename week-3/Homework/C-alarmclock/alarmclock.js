c20ec933b863090829977712dd6a89557c749779
diegok23 <dkogut@gmail.com>
2022-05-04 00:21:12 +0200

    Ejercicio completado

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,15 +1,63 @@
-function setAlarm() {}
+function setAlarm() {
+  let counter = document.querySelector('#alarmSet').value;
+  let timeInput = document.querySelector('#timeRemaining');
+  let minutes;
+  let seconds;
+
+  setMinutesAndSeconds();
+  const interval = setInterval(reverseCounter, 1000);
+
+  function reverseCounter() {
+    if (counter > 0) {
+      counter = counter - 1;
+      setMinutesAndSeconds();
+    } else {
+      clearInterval(interval);
+      playAlarm();
+    }
+  }
+
+  function setMinutesAndSeconds() {
+    if (counter < 60) {
+      minutes = '00';
+      seconds = counter;
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+    }
+    if (counter >= 60) {
+      if (counter % 60 === 0) {
+        seconds = '00';
+        minutes = counter / 60;
+        if (minutes < 10) {
+          minutes = '0' + minutes;
+        }
+      } else {
+        let minutesAndSeconds = (counter / 60).toString();
+        minutes = parseInt(minutesAndSeconds.split('.')[0]);
+        seconds = counter - minutes * 60;
+        if (minutes < 10) {
+          minutes = '0' + minutes;
+        }
+        if (seconds < 10) {
+          seconds = '0' + seconds;
+        }
+      }
+    }
+    timeInput.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+var audio = new Audio('alarmsound.mp3');
 
 function setup() {
-  document.getElementById("set").addEventListener("click", () => {
+  document.getElementById('set').addEventListener('click', () => {
     setAlarm();
   });
 
-  document.getElementById("stop").addEventListener("click", () => {
+  document.getElementById('stop').addEventListener('click', () => {
     pauseAlarm();
   });
 }
--- week-3/Homework/D-slideshow/slideshow.js
+++ week-3/Homework/D-slideshow/slideshow.js
@@ -1,1 +1,118 @@
-// Write your code here
+//DOM
+const carousel = document.createElement('div');
+carousel.className = 'carousel';
+document.body.appendChild(carousel);
+carousel.innerHTML =
+  '<div class="title"><h5></h5><h1>Image Slideshow </h1><h5>by DiegoIK</h5></div>' +
+  '<div class="content"><img class="image"></div>' +
+  '<div class="buttons">' +
+  '<button id="butAutoBck"><h3>Auto Back</h3></button>' +
+  '<button id="butBck"><h3>Back</h3></button>' +
+  '<button id="butStop"><h3>Stop</h3></button>' +
+  '<button id="butFwd"><h3>Forward</h3></button>' +
+  '<button id="butAutoFwd"><h3>Auto Forward</h3></button>' +
+  '</div>' +
+  '<div class="inputDiv"><p>Default slideshow time is 2.5 seconds, you can change in milliseconds</p><input class="input"></input></div>';
+
+const butFwd = document.querySelector('#butFwd');
+const butBck = document.querySelector('#butBck');
+const butAutoFwd = document.querySelector('#butAutoFwd');
+const butAutoBck = document.querySelector('#butAutoBck');
+const butStop = document.querySelector('#butStop');
+const image = document.querySelector('.image');
+const delayInput = document.querySelector('input');
+
+butFwd.addEventListener('click', stepForwards);
+butBck.addEventListener('click', stepBack);
+butStop.addEventListener('click', stop);
+butAutoFwd.addEventListener('click', callStepForwardsAuto);
+butAutoBck.addEventListener('click', callStepBackAuto);
+delayInput.addEventListener('change', timeToDelay);
+console.log(delayInput);
+//
+
+const images = ['./img/cat1.jpg', './img/cat2.jpg', './img/cat3.jpg', './img/cat4.jpg'];
+let indexImage = 0;
+let intervalId = undefined;
+let delay = 2500;
+image.src = images[0];
+
+console.log(delay);
+//FUNCTIONS
+
+function showImage() {
+  image.src = images[indexImage];
+}
+
+function clearInt() {
+  if (intervalId != undefined) {
+    window.clearInterval(intervalId);
+  }
+}
+
+function stop() {
+  window.clearInterval(intervalId);
+  indexImage = 0;
+  image.src = images[0];
+  delay = 2500;
+  delayInput.value = '';
+}
+
+function callStepForwardsAuto() {
+  clearInt();
+  intervalId = stepForwardsAuto();
+  console.log(intervalId);
+}
+
+function callStepBackAuto() {
+  clearInt();
+  intervalId = stepBackAuto();
+  console.log(intervalId);
+}
+
+function stepForwards() {
+  clearInt();
+  if (indexImage == images.length - 1) {
+    indexImage = 0;
+  } else {
+    indexImage++;
+  }
+  showImage();
+}
+
+function stepBack() {
+  clearInt();
+  if (indexImage == 0) {
+    indexImage = images.length - 1;
+  } else {
+    indexImage--;
+  }
+  showImage();
+}
+
+function stepForwardsAuto() {
+  return setInterval(() => {
+    if (indexImage == images.length - 1) {
+      indexImage = 0;
+    } else {
+      indexImage++;
+    }
+    showImage();
+  }, delay);
+}
+
+function stepBackAuto() {
+  return setInterval(() => {
+    if (indexImage == 0) {
+      indexImage = images.length - 1;
+    } else {
+      indexImage--;
+    }
+    showImage();
+  }, delay);
+}
+
+function timeToDelay(event) {
+  delay = event.target.value;
+  return delay;
+}
